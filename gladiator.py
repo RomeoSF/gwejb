cce582c4266096617f40816d9506bd1d2ccac77b
RomeoSF <143185658+RomeoSF@users.noreply.github.com>
2024-10-11 15:12:12 +0200

    Attack funktion för spelare utökad, bas attack funktion för fiende påbörjad

--- gladiator.py
+++ gladiator.py
@@ -1,9 +1,15 @@
+import random
+
+# Olika karaktärer i spelet
+
 class Karaktärer:
     def __init__(self, namn, hälsa):
         self.namn = namn
         self.hälsa = hälsa
         
 karaktär1 = Karaktärer("Jöns", 100)
+
+# Olika attacker i spelet
 
 attack = []
 
@@ -16,18 +22,43 @@ class Attacker:
 jabb = Attacker("jabb", 10)
 spark = Attacker("spark", 15)
 
+# Fiendens hälsa
 
-# Spel funktion för att spelare ska attackera
+fie_hälsa = 100
 
+# Funktion för spelarens attack
 def din_att():
-    att_val = input("Välj din attack: ")
-    giltig_attack = None
-    # Kolla attacker i attack lista
-    for att in attack:
-        if att_val.lower() == att.namn.lower():
-            giltig_attack = att
+    global fie_hälsa
+    while True:
+        att_val = input("Välj din attack: ")
+        giltig_attack = None
+        # Kollar så att svaret är gitligt
+        for att in attack:
+            if att_val.lower() == att.namn.lower():
+                giltig_attack = att
+                break
+        if giltig_attack:
+            attack_namn = giltig_attack.namn
+            attack_skada = giltig_attack.skada
             break
-    if giltig_attack:
-        print(f"Du valde {att.namn}")
+        else:
+            print("Ogitligt svar, försök igen")
+    if karaktär1.hälsa and fie_hälsa > 0:
+        fie_hälsa -= attack_skada
 
-din_att()
+    
+    
+        
+
+    
+    
+
+
+#   Funktion för fiendens attack
+
+def fiende_att():
+    fie_att = random.choice(attack)
+  
+
+
+#fiende_att()
